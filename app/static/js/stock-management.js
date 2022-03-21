ded9aef387551a1509b09c662aaf185daa46368a
Chenyu Guo <chenyu.guo1@ucdconnect.ie>
2022-03-21 19:59:56 +0800

    ajax connection of product & model remove function

--- app/static/js/stock-management.js
+++ app/static/js/stock-management.js
@@ -3,35 +3,53 @@
 */
 
 //remove a specific product, using ajax
-function remove_product(product_id){
-    $.post("/api/stock-management/remove-product", {
-        "product_id": product_id
+function remove_product(product_id)
+{
+    let targetName = "product-section-" + product_id;
+    let target = document.getElementById(targetName);
 
-    }).done(function (response){
-        // get from server (backend)
-        let returnValue = response['returnValue'];
+    $.post("/api/stock-management/remove-product",
+        {
+            "product_id": product_id
+        }).done(function (response)
+            {
+                // get from server (backend)
+                let returnValue = response['returnValue'];
 
-        if (returnValue === 0) { //success
-            //respond the removal in the frontend page
-            // ...
-        }
-
-    });
+                if (returnValue === 0)
+                {
+                    //success
+                    console.log("Product " + product_id + "removed");
+                    target.remove();
+                }
+            });
 }
 
 //remove a specific model type, using ajax
-function remove_model_type(model_type_id){
-    $.post("/api/stock-management/remove-model-type", {
-        "model_type_id": model_type_id
+function remove_model_type(model_type_id)
+{
+    let targetName = "modeltype-section-" + model_type_id;
+    let target = document.getElementById(targetName);
 
-    }).done(function (response){
-        // get from server (backend)
-        let returnValue = response['returnValue'];
+    console.log(target);
 
-        if (returnValue === 0) { //success
-            //respond the removal in the frontend page
-            // ...
-        }
+    console.log("remove model_type: " + targetName);
+    $.post("/api/stock-management/remove-model-type",
+        {
+            "model_type_id": model_type_id
+        }).done(function (response)
+            {
+                console.log('delete get replied')
+                // get from server (backend)
+                let returnValue = response['returnValue'];
 
-    });
+                console.log(returnValue);
+
+                if (returnValue === 0)
+                {
+                    //success
+                    console.log("ModelType " + model_type_id + "removed");
+                    target.remove();
+                }
+            });
 }
