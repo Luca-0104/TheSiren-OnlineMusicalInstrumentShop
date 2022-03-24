04c859af74c614f5c8cebf7a0c7653bd818ab24b
Tian Tian <tian.tian1@ucdconnect.ie>
2022-03-24 20:06:00 +0800

    Upload product updated again.

--- app/static/js/management-system.js
+++ app/static/js/management-system.js
@@ -5,53 +5,13 @@
     $(document).ready(function (){
         $("#add-model-type").click(function (){
 
-            let repeat_model_name = ["<div class=\"col-md-12\">",
-                            "      <div class=\"form-group\">",
-                            "              <label>Model Name *</label>",
-                            "                     <input name=\"",counter,"_model_name\" type=\"text\" class=\"form-control\" placeholder=\"Enter Name\" data-errors=\"Please Enter Name.\">",
-                            "                      <div class=\"help-block with-errors\"></div>",
-                            "        </div>",
-                            "  </div>"].join("");
-            let repeat_model_number = ["<div class=\"col-md-12\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Serial Number *</label>",
-            "                                            <input name=\"",counter,"_model_num\" type=\"text\" class=\"form-control\" placeholder=\"Enter Serial Number\" data-errors=\"Please Enter Serial Number.\">",
-            "                                            <div class=\"help-block with-errors\"></div>",
-            "                                        </div>",
-            "                                    </div>"].join("");
-            let repeat_model_price = ["<div class=\"col-md-6\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Price *</label>",
-            "                                            <input name=\"",counter,"_model_price\" type=\"text\" class=\"form-control\" placeholder=\"Enter Price\" data-errors=\"Please Enter Price.\">",
-            "                                            <div class=\"help-block with-errors\"></div>",
-            "                                        </div>",
-            "                                    </div>"].join("");
-            let repeat_model_stock = ["<div class=\"col-md-6\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Stock *</label>",
-            "                                            <input name=\"",counter,"_model_stock\" type=\"text\" class=\"form-control\" placeholder=\"Enter Stock\" data-errors=\"Please Enter Stock.\">",
-            "                                            <div class=\"help-block with-errors\"></div>",
-            "                                        </div>",
-            "                                    </div>"].join("");
-            let repeat_model_intro_pic = ["<div class=\"col-md-12\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Thumbnail *</label>",
-            "                                            <input name=\"",counter,"_model_intro_pic\" type=\"file\" class=\"form-control image-file\" name=\"pic\" accept=\"image/*\" multiple=\"multiple\">",
-            "                                        </div>",
-            "                                    </div>"].join("");
-            let repeat_model_pic = ["<div class=\"col-md-12\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Product Pictures *</label>",
-            "                                            <input name=\"",counter,"_model_pic\" type=\"file\" class=\"form-control image-file\" name=\"pic\" accept=\"image/*\" multiple=\"multiple\">",
-            "                                        </div>",
-            "                                    </div>"].join("");
-            let repeat_model_description = ["<div class=\"col-md-12\">",
-            "                                        <div class=\"form-group\">",
-            "                                            <label>Description / Product Details</label>",
-            "                                            <textarea name=\"",counter,"_model_description\" class=\"form-control\" rows=\"4\"></textarea>",
-            "                                        </div>",
-            "                                    </div>"].join("");
-
+            let repeat_model_name;
+            let repeat_model_number;
+            let repeat_model_price;
+            let repeat_model_stock;
+            let repeat_model_intro_pic;
+            let repeat_model_pic;
+            let repeat_model_description;
 
             let prepend_content = "<div class=\"card-footer col-md-12\">\n" +
                     "                                       <div id=\"1_model\">\n" +
@@ -61,6 +21,52 @@
                 if(counter === 1){
                     repeat_model_type.before(prepend_content);
                     counter += 1;
+                    repeat_model_name = ["<div class=\"col-md-12\">",
+                            "      <div class=\"form-group\">",
+                            "              <label>Model Name *</label>",
+                            "                     <input name=\"",counter,"_model_name\" type=\"text\" class=\"form-control\" placeholder=\"Enter Name\" data-errors=\"Please Enter Name.\">",
+                            "                      <div class=\"help-block with-errors\"></div>",
+                            "        </div>",
+                            "  </div>"].join("");
+                   repeat_model_number = ["<div class=\"col-md-12\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Serial Number *</label>",
+            "                                            <input name=\"",counter,"_model_num\" type=\"text\" class=\"form-control\" placeholder=\"Enter Serial Number\" data-errors=\"Please Enter Serial Number.\">",
+            "                                            <div class=\"help-block with-errors\"></div>",
+            "                                        </div>",
+            "                                    </div>"].join("");
+                   repeat_model_price = ["<div class=\"col-md-6\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Price *</label>",
+            "                                            <input name=\"",counter,"_model_price\" type=\"text\" class=\"form-control\" placeholder=\"Enter Price\" data-errors=\"Please Enter Price.\">",
+            "                                            <div class=\"help-block with-errors\"></div>",
+            "                                        </div>",
+            "                                    </div>"].join("");
+                   repeat_model_stock = ["<div class=\"col-md-6\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Stock *</label>",
+            "                                            <input name=\"",counter,"_model_stock\" type=\"text\" class=\"form-control\" placeholder=\"Enter Stock\" data-errors=\"Please Enter Stock.\">",
+            "                                            <div class=\"help-block with-errors\"></div>",
+            "                                        </div>",
+            "                                    </div>"].join("");
+                  repeat_model_intro_pic = ["<div class=\"col-md-12\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Thumbnail *</label>",
+            "                                            <input name=\"",counter,"_model_intro_pic\" type=\"file\" class=\"form-control image-file\" name=\"pic\" accept=\"image/*\" multiple=\"multiple\">",
+            "                                        </div>",
+            "                                    </div>"].join("");
+                  repeat_model_pic = ["<div class=\"col-md-12\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Product Pictures *</label>",
+            "                                            <input name=\"",counter,"_model_pic\" type=\"file\" class=\"form-control image-file\" name=\"pic\" accept=\"image/*\" multiple=\"multiple\">",
+            "                                        </div>",
+            "                                    </div>"].join("");
+                  repeat_model_description = ["<div class=\"col-md-12\">",
+            "                                        <div class=\"form-group\">",
+            "                                            <label>Description / Product Details</label>",
+            "                                            <textarea name=\"",counter,"_model_description\" class=\"form-control\" rows=\"4\"></textarea>",
+            "                                        </div>",
+            "                                    </div>"].join("");
                     prepend_content = "<div class=\"card-footer col-md-12\">\n" +
                     "                                       <div id=\"1_model\">\n" +
                     "                                            <h4 class=\"card-title\">" + counter + ".</h4>\n" +
