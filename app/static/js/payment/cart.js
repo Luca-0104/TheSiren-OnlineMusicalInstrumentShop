e39390af9df76978bd641759dc71fbd41740383a
Chenyu Guo <chenyu.guo1@ucdconnect.ie>
2022-03-26 13:24:34 +0800

    temp

---
+++ app/static/js/payment/cart.js
@@ -0,0 +1,13 @@
+
+function calculateTotalCost()
+{
+    let totalRow = $("#cart_table").attr("total_row");
+    let totalCost = 0;
+    for(let i=1; i<totalRowh; i++)
+    {
+        if($("#select_"+i).checked)
+        {
+            totalCost = totalCost + $()
+        }
+    }
+}
