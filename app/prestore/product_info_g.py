7fbd2d012650ed98a3948787ef2d730f174ca81b
Geng <tianyi.geng@ucdconnect.ie>
2022-04-07 00:05:41 +0800

    product

--- app/prestore/product_info_g.py
+++ app/prestore/product_info_g.py
@@ -41,18 +41,119 @@ Category:
 pm_lst = [
     # product 1 ----------------------------------------------------
     [
-        "product-name-test",        # product name
-        1,                          # brand id
-        1,                          # cate id - C
-        7,                          # cate id - T
-        53,                         # cate id - A
-        [                           # model type list
+        "Stentor Amati model violin",  # product name
+        1,  # Stentor
+        1,  # cate id - C
+        1,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
             # model 1 ----------------------------------------------------------
             [
-                'model-name-test',          # model name
-                'model-description-test',   # m description
-                100,                        # m price
-                2,                          # kg
+                'Stentor Amati model violin',  # model name
+                'A beautiful violin modelled on an original violin of 1694 by Nicola Amati. It is made fom selected and figured European tonewoods and is finished with a hand applied traditional varnish which brings out the appearance of the wood. This superb handcrafted instrument features the deeper arching of Amati style. It is finished in a warm golden base colour with traditional hand applied shellac varnish. It has Pirastro strings and is individually workshop fitted.',  # m description
+                12490,  # m price
+                1.5,  # kg
+                [
+                    'g-p1m1-1.jpg',
+                    'g-p1m1-2.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 2 ----------------------------------------------------
+    [
+        "Stentor Arcadia viola",  # product name
+        1,  # Stentor
+        1,  # cate id - C
+        2,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Stentor Arcadia viola',  # model name
+                "The Stentor Arcadia is the top of Stentor's range of high grade orchestral models. These are superb handcrafted instruments, carved from the most select, highly figured tonewoods. The Stentor Arcadia viola is made in a specialist hand workshop within the Stentor factory, where the most skilled individual workers produce under the close supervision of a master maker, in a traditional luthier's environment that would be familiar to makers throughout the centuries. The superior sound is unique to each instrument and reflects the individuality of each one. The viola is finished with a hand-applied traditional shellac varnish and fitted with Pirastro strings. All instruments are individually workshop fitted.",  # m description
+                23100,  # m price
+                2,  # kg
+                [
+                    'g-p2m1-1.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+    # product 3 ----------------------------------------------------
+    [
+        "Stentor Arcadia cello",  # product name
+        1,  # Stentor
+        1,  # cate id - C
+        3,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Stentor Arcadia cello',  # model name
+                'The Stentor Arcadia is the top of Stentor’s range of high-grade orchestral models. These are superb handcrafted instruments, carved from the most select, highly figured tonewoods. The Stentor Arcadia cello is made in a specialist hand workshop within the Stentor factory. This individual cello is finished with a hand-applied traditional shellac varnish and fitted with professional quality branded strings. All instruments are individually workshop fitted.',  # m description
+                33600,  # m price
+                7.5,  # kg
+                [
+                    'g-p3m1-1.jpg',
+                    'g-p3m1-2.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+    # product 4 ----------------------------------------------------
+    [
+        "Stentor Arcadia double bass",  # product name
+        1,  # Stentor
+        1,  # cate id - C
+        4,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Stentor Arcadia double bass',  # model name
+                "The Stentor Arcadia double bass is an excellent handcrafted instrument, hand carved from selected solid tonewoods, with inlaid purfling. It features a fully carved well flamed back and front and is finished with a hand applied traditional shellac varnish. Each instrument is individually workshop fitted. The Stentor Arcadia is the top of Stentor's range of high-grade orchestral models. These are superb handcrafted instruments, carved from selected and figured tonewoods. It is made in a specialist bass workshop within the Stentor factory, in a traditional luthier's environment that would be familiar to makers throughout the centuries. This produces fine instruments with a superior finish and sound that reflects the making of each instrument.",  # m description
+                35900,  # m price
+                5.5,  # kg
+                [
+                    'g-p4m1-1.png',
+                    'g-p4m1-2.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 5 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
                 [
                     'p1m1-1.png',
                     'p1m1-2.png',
@@ -66,6 +167,138 @@ pm_lst = [
         ]
     ],
 
-    # product 1 ----------------------------------------------------
-    # []
+    # product 6 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
+                [
+                    'p1m1-1.png',
+                    'p1m1-2.png',
+                    'p1m1-3.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 7 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
+                [
+                    'p1m1-1.png',
+                    'p1m1-2.png',
+                    'p1m1-3.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 4 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
+                [
+                    'p1m1-1.png',
+                    'p1m1-2.png',
+                    'p1m1-3.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 4 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
+                [
+                    'p1m1-1.png',
+                    'p1m1-2.png',
+                    'p1m1-3.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 4 ----------------------------------------------------
+    [
+        "product-name-test",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        7,  # cate id - T
+        53,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'model-name-test',  # model name
+                'model-description-test',  # m description
+                100,  # m price
+                2,  # kg
+                [
+                    'p1m1-1.png',
+                    'p1m1-2.png',
+                    'p1m1-3.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
 ]
