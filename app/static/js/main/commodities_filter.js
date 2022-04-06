b55400b15707b7f03ce960df0afaf1e6b4f56a51
Tian Tian <tian.tian1@ucdconnect.ie>
2022-04-06 21:38:27 +0800

    For all commodities and commodity details modification.

--- app/static/js/main/commodities_filter.js
+++ app/static/js/main/commodities_filter.js
@@ -6,30 +6,23 @@ let checking_numbers = [7,48,5,6];
 
 function initialize_checkbox(param){
     let p = 0;
-    if (param === "#check_class_"){
-        p = 0
-    }
-    else if(param === "#check_type_"){}
-    else if(param === "#check_add_"){}
-    else if(param === "#check_brand_"){}
-    for (let i=1; i<=checking_numbers[0]; i++){
-         let init_select = $('#check_class_'+i);
+    if     (param === "#check_class_"){p = 0;}
+    else if (param === "#check_type_"){p = 1;}
+    else if  (param === "#check_add_"){p = 2;}
+    else if(param === "#check_brand_"){p = 3;}
+    for (let i=1; i<=checking_numbers[p]; i++){
+         let init_select = $(param+i);
          init_select.click();
-
     }
 }
 
 $(document).ready(function (){
 
-     for (let i=1; i<=checking_numbers[0]; i++){
-         let init_select = $('#check_class_'+i);
-         init_select.click();
+     initialize_checkbox("#check_class_");
+     initialize_checkbox("#check_type_");
+     initialize_checkbox("#check_add_");
+     initialize_checkbox("#check_brand_");
 
-     }
-     for (let i=1; i<=checking_numbers[1]; i++){
-         let init_select = $('#check_type_'+i);
-         init_select.click();
-     }
      $('.category').on("change", function (){
          console.log($(this).attr('id'));
 
@@ -38,13 +31,13 @@ $(document).ready(function (){
          console.log(p);
 
          if (!$(this).prop('checked')){
-             console.log("############");
+
              for (let i = 1; i <= checking_numbers[0]; i++){
                  if(p !== i){
                      let cancel_select = $('#check_class_'+i);
                      cancel_select.attr("class","checkbox category");
                      cancel_select.prop("checked","off");
-                     console.log("@@@@@@@@@");
+
                  }
              }
              $(this).attr("class","checkbox w--redirected-checked category");
@@ -54,6 +47,29 @@ $(document).ready(function (){
          }
      });
 
+     $('.category').on("change", function (){
+         console.log($(this).attr('id'));
+
+         console.log($(this).attr("checked"));
+         let p = $(this).attr('id').split('_').pop();
+         console.log(p);
+
+         if (!$(this).prop('checked')){
+
+             for (let i = 1; i <= checking_numbers[0]; i++){
+                 if(p !== i){
+                     let cancel_select = $('#check_class_'+i);
+                     cancel_select.attr("class","checkbox category");
+                     cancel_select.prop("checked","off");
+
+                 }
+             }
+             $(this).attr("class","checkbox w--redirected-checked category");
+             $(this).prop("checked","on");
+
+             console.log($(this).attr("checked")+'2');
+         }
+     });
 
      // $('#category_checkbox').on("change",function (){
      //    for (let i=1; i<=7; i++){
