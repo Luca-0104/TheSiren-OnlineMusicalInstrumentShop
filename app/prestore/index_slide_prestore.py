4f7a64bce24a4f59ca10d47cccb0ae02b6b2e205
Geng <tianyi.geng@ucdconnect.ie>
2022-04-11 17:36:40 +0800

    index

--- app/prestore/index_slide_prestore.py
+++ app/prestore/index_slide_prestore.py
@@ -1,4 +1,4 @@
-pm_lst = [
+pm_lst_index = [
     # product 1 ----------------------------------------------------
     [
         "Gibson THE LEO SCALA SUPER '58 FLYING Vs",  # product name
@@ -26,17 +26,73 @@ pm_lst = [
             # model 2 ----------------------------------------------------------
             [
                 "Gibson THE LEO SCALA SUPER '58 FLYING Vs THE 777 SERIES",  # model name
-                'Solid Korina Body and Long Neck Tenon. Brazilian Rosewood Fingerboard. Hand Lacquered and Aged by the Artisan. Custom Wound “Retrophonic’Humbucker Pickup. Tune-O-Matic Bridge with 1.5" Brass Studs.  50s Wiring with Master Volume and Master Tone. Custom Bumblebee Capacitors. Custom Dogear Pickup Rings.',
+                'Solid Korina Body and Long Neck Tenon. Brazilian Rosewood Fingerboard. Hand Lacquered and Aged by '
+                'the Artisan. Custom Wound "Retrophonic" Humbucker Pickup. Tune-O-Matic Bridge with 1.5" Brass Studs. '
+                '50s Wiring with Master Volume and Master Tone Controls. Bigsby B7 Vibrato',
                 # m description
-                38000,  # m price
+                39000,  # m price
                 2.4,  # kg
                 [
-                    'p1m1-1.jpg',
-                    'p1m1-2.jpg',
-                    'p1m1-3.jpg',
-                    'p1m1-4.jpg',
-                    'p1m1-5.jpg'
+                    'p1m2-1.jpg',
+                    'p1m2-2.jpg',
+                    'p1m2-3.jpg',
+                    'p1m2-4.jpg',
+                    'p1m2-5.jpg'
 
+                ]
+            ],
+            # model 3 ----------------------------------------------------------
+            [
+                "Gibson THE LEO SCALA SUPER '58 FLYING Vs THE SERAPH",  # model name
+                'Darker Solid Korina Body and Neck Brazilian Rosewood Fingerboard. Hand Aging and Relicing by the Artisan. Graphical Treatment to the Back of the Body Single Hand-Wound Custom Pickup. Bespoke Wiring with Three Knobs and Switch Variable Coil Split Wiring. Custom Bumblebee Capacitors. Schaller Roller Tune-0-Matic Bridge BigsbyRB7 Vibrato Tailpiece. AMetal Double Symmetrical Pickguard with a 1937 Pontiac Engraving',
+                # m description
+                43000,  # m price
+                2.4,  # kg
+                [
+                    'p1m3-1.jpg',
+                    'p1m3-2.jpg',
+                    'p1m3-3.jpg',
+                    'p1m3-4.jpg',
+                    'p1m3-5.jpg'
+
+                ]
+            ]
+        ]
+    ],
+
+    # product 2 ----------------------------------------------------
+    [
+        "BOSS’s Legendary Super Overdrive Sound Effectors",  # product name
+        13,  # brand id
+        6,  # cate id - C
+        49,  # cate id - T
+        54,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'BOSS’s Legendary Super Overdrive Sound Effectors SD-1',  # model name
+                'Since its release in 1981, the workhorse SD-1 Super Overdrive has been the core gain pedal for generations of players across every musical genre. Based around the revolutionary asymmetrical clipping circuit from the OD-1 Overdrive—one of the three original BOSS compact pedals from 1977—the SD-1 delivers rich, smooth, and highly musical overdrive tones that continue to inspire guitarists everywhere.',
+                # m description
+                99,  # m price
+                0.5,  # kg
+                [
+                    'p2m1-1.jpg',
+                    'p2m1-2.jpg',
+                    'p2m1-3.jpg',
+                    'p2m1-4.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            [
+                'BOSS’s Limited Edition 40th Anniversary SD-1 Super Overdrive (SD-1-4A) ',  # model name
+                "To celebrate 40 years of continuous SD-1 production, we've created a limited edition version that's available throughout 2021. This commemorative pedal features a black body with yellow lettering, gold-capped knobs, and a vintage silver thumbscrew for the battery compartment and comes in a box with a special 40th Anniversary logo.",
+                # m description
+                99,  # m price
+                0.5,  # kg
+                [
+                    'p2m2-1.jpg',
+                    'p2m2-2.jpg',
+                    'p2m2-3.jpg'
                 ]
             ],
             # model 3 ----------------------------------------------------------
@@ -44,6 +100,104 @@ pm_lst = [
         ]
     ],
 
-    # product 1 ----------------------------------------------------
-    # []
+    # product 3 ----------------------------------------------------
+    [
+        "Lowden GL10 Guitar",  # product name
+        1,  # brand id
+        1,  # cate id - C
+        11,  # cate id - T
+        54,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Lowden GL10 Guitar',  # model name
+                'The GL-10 was first introduced back in 2018 and embodies the classic Lowden character, drawing from over 40 years of guitar making heritage to invite the player to explore new tonal possibilities that await.',
+                # m description
+                4999,  # m price
+                2,  # kg
+                [
+                    'p3m1-1.png'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            [
+                'Lowden GL10 2022 Concept Guitar',  # model name
+                'The GL-10 was first introduced back in 2018 and embodies the classic Lowden character, drawing from over 40 years of guitar making heritage to invite the player to explore new tonal possibilities that await. NEW Semi-hollow one piece Brazilian Mahogany body Scalloped solid AAAA grade top',
+                # m description
+                4999,  # m price
+                2,  # kg
+                [
+                    'p3m2-1.png'
+                ]
+            ],
+            # model 3 ----------------------------------------------------------
+            [
+                'Lowden GL10C Guitar',  # model name
+                'The GL-10 was first introduced back in 2018 and embodies the classic Lowden character, drawing from over 40 years of guitar making heritage to invite the player to explore new tonal possibilities that await. Internalmicro-chambers for reduced weight over the standard GL-10 Solid AAAA grade top',
+                # m description
+                4999,  # m price
+                2,  # kg
+                [
+                    'p3m3-1.png'
+                ]
+            ]
+        ]
+    ],
+    # product 4 ----------------------------------------------------
+    [
+        "Roland TD-27KV V-Drums",  # product name
+        17,  # brand id
+        4,  # cate id - C
+        45,  # cate id - T
+        54,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Roland TD-27KV V-Drums',  # model name
+                'The mid-level TD-27KV delivers the immersive sound and response that you only get with V-Drums. At its heart is the powerful new TD-27 sound engine, filled with premium-grade, fully-customizable drum and cymbal sounds. Advanced digital trigger technology detects every playing nuance in ultra-high definition, while large-diameter drum and cymbal pads offer exceptional feel, presented in a spacious acoustic-style layout.',
+                # m description
+                4590,  # m price
+                15,  # kg
+                [
+                    'p4m1-1.jpg',
+                    'p4m1-2.jpg',
+                    'p4m1-3.jpg',
+                    'p4m1-4.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+    # product 5 ----------------------------------------------------
+    [
+        "Roland A-88MKII MIDI Keyboard Controller",  # product name
+        17,  # brand id
+        5,  # cate id - C
+        46,  # cate id - T
+        54,  # cate id - A
+        [  # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'Roland A-88MKII MIDI Keyboard Controller',  # model name
+                "The latest in our long line of professional MIDI controllers, the Roland A-88MKII is supremely playable, with onboard creative tools for today's musicians and producers. Our acclaimed hammer-action keyboard and built-to-last quality combine with modern features like USB-C connectivity, RGB-lit controls, and MIDI 2.0 (coming soon) for the best performance in its class.",
+                # m description
+                3999,  # m price
+                4,  # kg
+                [
+                    'p5m1-1.jpg',
+                    'p5m1-2.jpg',
+                    'p5m1-3.jpg',
+                    'p5m1-4.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            # [],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ]
+
 ]
