b1bb64e5d3763a55607fc97681e2ced1c8112184
TAN TIAN <tian.tan@ucdconnect.ie>
2022-04-12 00:05:20 +0800

    snare drum product & module & current image resized

--- app/prestore/product_info_t.py
+++ app/prestore/product_info_t.py
@@ -32,7 +32,7 @@ pm_lst = [
             ],
             # model 2 ----------------------------------------------------------
             [
-                'CB-9032',          # model name
+                'CB-9036',          # model name
                 'A new shell design employs maple for a solid low end with complex, delicate sustain. Solid steel hoops provide a well-defined tonal core that cannot be achieved with wood hoops. A dark wood finish exudes sophistication and elegance. CB-9000 Series features Yamaha Remo Nuskyn heads on both sides. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (Large)',   # m description
                 13000,                        # m price
                 24.7,                          # kg
@@ -43,6 +43,164 @@ pm_lst = [
             ],
             # model 3 ----------------------------------------------------------
             # []
+        ]
+    ],
+
+    # product 2
+    [
+        "CB-8000 Series",        # product name  # Yamaha
+        16,                          # brand id
+        4,                          # cate id - C
+        37,                          # cate id - T
+        53,                         # cate id - A
+        [                           # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'CB-8032',          # model name
+                'A new shell design employs maple for a solid low end with complex, delicate sustain. Solid steel hoops provide a well-defined tonal core that cannot be achieved with wood hoops. A dark wood finish exudes sophistication and elegance. CB-8000 Series features Yamaha Remo Nuskyn heads on both sides. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (Small)',   # m description
+                9000,                        # m price
+                18.9,                          # kg
+                [
+                    't-p2m1-1.jpg',
+                    't-p2m1-2.jpg'
+                ]
+            ],
+            # model 2 ----------------------------------------------------------
+            [
+                'CB-8036',          # model name
+                'A new shell design employs maple for a solid low end with complex, delicate sustain. Solid steel hoops provide a well-defined tonal core that cannot be achieved with wood hoops. A dark wood finish exudes sophistication and elegance. CB-8000 Series features Yamaha Remo Nuskyn heads on both sides. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (Large)',   # m description
+                12000,                        # m price
+                23.3,                          # kg
+                [
+                    't-p2m2-1.jpg',
+                    't-p2m2-2.jpg'
+                ]
+            ],
+            # model 3 ----------------------------------------------------------
+            # []
+        ]
+    ],
+
+    # product 3
+    [
+        "CB-7000 Series",        # product name  # Yamaha
+        16,                          # brand id
+        4,                          # cate id - C
+        37,                          # cate id - T
+        53,                         # cate id - A
+        [                           # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'CB-7024',          # model name
+                'Every aspect of the shell, from material to design, has been reviewed and upgraded in the pursuit of superior sound. A dark wood finish exudes sophistication and elegance. Yamaha Remo Nuskyn playing heads are supplied on 32" and 36" CB-7000 Series models. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (8-Lugs)',   # m description
+                8000,                        # m price
+                8.3,                          # kg
+                [
+                    't-p3m1-1.jpg',
+                    't-p3m1-2.jpg'
+                ]
+            ],
+
+            # model 2 ----------------------------------------------------------
+            [
+                'CB-7028',          # model name
+                'Every aspect of the shell, from material to design, has been reviewed and upgraded in the pursuit of superior sound. A dark wood finish exudes sophistication and elegance. Yamaha Remo Nuskyn playing heads are supplied on 32" and 36" CB-7000 Series models. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (10-Lugs)',   # m description
+                10000,                        # m price
+                9.8,                          # kg
+                [
+                    't-p3m2-1.jpg',
+                    't-p3m2-2.jpg'
+                ]
+            ],
+
+            # model 3 ----------------------------------------------------------
+            [
+                'CB-7032',          # model name
+                'Every aspect of the shell, from material to design, has been reviewed and upgraded in the pursuit of superior sound. A dark wood finish exudes sophistication and elegance. Yamaha Remo Nuskyn playing heads are supplied on 32" and 36" CB-7000 Series models. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (12-Lugs)',   # m description
+                12000,                        # m price
+                12.4,                          # kg
+                [
+                    't-p3m3-1.jpg',
+                    't-p3m3-2.jpg'
+                ]
+            ],
+
+            # model 4 ----------------------------------------------------------
+            [
+                'CB-7036',          # model name
+                'Every aspect of the shell, from material to design, has been reviewed and upgraded in the pursuit of superior sound. A dark wood finish exudes sophistication and elegance. Yamaha Remo Nuskyn playing heads are supplied on 32" and 36" CB-7000 Series models. These high-quality heads offer attack and sustain that is similar to natural skin, along with deep, rich resonance. (12-Lugs)',   # m description
+                12000,                        # m price
+                13.8,                          # kg
+                [
+                    't-p3m4-1.jpg',
+                    't-p3m4-2.jpg'
+                ]
+            ],
+        ]
+    ],
+
+    # product 4
+    [
+        "Live Custom Hybrid Oak",        # product name  # Yamaha
+        16,                          # brand id
+        4,                          # cate id - C
+        38,                          # cate id - T
+        53,                         # cate id - A
+        [                           # model type list
+            # model 1 ----------------------------------------------------------
+            [
+                'UZU Ice Sunburst',          # model name
+                'The Live Custom Hybrid Oak shell concept is borrowed from our flagship PHX and Absolute Hybrid Maple kits, which feature a dense phenolic sheet sandwiched between two sheets of Oak ply. Always a pioneer of Oak drums, Yamaha has enhanced the dynamic range of the new Live Custom kits by accentuating their attack and projection. Shells feature Yamaha exclusive Absolute Lugs. Their sophisticated design adds a touch of class to the drums. The heavy-duty Q-type strainer is robust whilst offering solid, stable and smooth operation, along with adjustment and snare replacement. Thicker steel hoops offer a tighter sound and sharper response. (UZU Ice Sunburst)',   # m description
+                9500,                        # m price
+                9.8,                          # kg
+                [
+                    't-p4m1-1.jpg'
+                ]
+            ],
+
+            # model 2 ----------------------------------------------------------
+            [
+                'UZU Magma Sunburst',          # model name
+                'The Live Custom Hybrid Oak shell concept is borrowed from our flagship PHX and Absolute Hybrid Maple kits, which feature a dense phenolic sheet sandwiched between two sheets of Oak ply. Always a pioneer of Oak drums, Yamaha has enhanced the dynamic range of the new Live Custom kits by accentuating their attack and projection. Shells feature Yamaha exclusive Absolute Lugs. Their sophisticated design adds a touch of class to the drums. The heavy-duty Q-type strainer is robust whilst offering solid, stable and smooth operation, along with adjustment and snare replacement. Thicker steel hoops offer a tighter sound and sharper response. (UZU Magma Sunburst)',   # m description
+                9500,                        # m price
+                9.8,                          # kg
+                [
+                    't-p4m2-1.jpg'
+                ]
+            ],
+
+            # model 3 ----------------------------------------------------------
+            [
+                'UZU Natural',          # model name
+                'The Live Custom Hybrid Oak shell concept is borrowed from our flagship PHX and Absolute Hybrid Maple kits, which feature a dense phenolic sheet sandwiched between two sheets of Oak ply. Always a pioneer of Oak drums, Yamaha has enhanced the dynamic range of the new Live Custom kits by accentuating their attack and projection. Shells feature Yamaha exclusive Absolute Lugs. Their sophisticated design adds a touch of class to the drums. The heavy-duty Q-type strainer is robust whilst offering solid, stable and smooth operation, along with adjustment and snare replacement. Thicker steel hoops offer a tighter sound and sharper response. (UZU Natural)',   # m description
+                9500,                        # m price
+                9.8,                          # kg
+                [
+                    't-p4m3-1.jpg'
+                ]
+            ],
+
+            # model 4 ----------------------------------------------------------
+            [
+                'UZU Earth Sunburst',          # model name
+                'The Live Custom Hybrid Oak shell concept is borrowed from our flagship PHX and Absolute Hybrid Maple kits, which feature a dense phenolic sheet sandwiched between two sheets of Oak ply. Always a pioneer of Oak drums, Yamaha has enhanced the dynamic range of the new Live Custom kits by accentuating their attack and projection. Shells feature Yamaha exclusive Absolute Lugs. Their sophisticated design adds a touch of class to the drums. The heavy-duty Q-type strainer is robust whilst offering solid, stable and smooth operation, along with adjustment and snare replacement. Thicker steel hoops offer a tighter sound and sharper response. (UZU Earth Sunburst)',   # m description
+                9500,                        # m price
+                9.8,                          # kg
+                [
+                    't-p4m4-1.jpg'
+                ]
+            ],
+
+            # model 5 ----------------------------------------------------------
+            [
+                'UZU Charcoal Sunburst',          # model name
+                'The Live Custom Hybrid Oak shell concept is borrowed from our flagship PHX and Absolute Hybrid Maple kits, which feature a dense phenolic sheet sandwiched between two sheets of Oak ply. Always a pioneer of Oak drums, Yamaha has enhanced the dynamic range of the new Live Custom kits by accentuating their attack and projection. Shells feature Yamaha exclusive Absolute Lugs. Their sophisticated design adds a touch of class to the drums. The heavy-duty Q-type strainer is robust whilst offering solid, stable and smooth operation, along with adjustment and snare replacement. Thicker steel hoops offer a tighter sound and sharper response. (UZU Charcoal Sunburst)',   # m description
+                9500,                        # m price
+                9.8,                          # kg
+                [
+                    't-p4m5-1.jpg'
+                ]
+            ],
         ]
     ],
 
