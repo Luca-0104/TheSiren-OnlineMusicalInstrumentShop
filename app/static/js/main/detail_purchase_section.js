e5c193fdb1844874e896335810b6cc1c6499493c
Luca <zhe.liu1@ucdconnect.ie>
2022-04-27 13:41:21 +0800

    detail page quantity check and condition of out of stock

--- app/static/js/main/detail_purchase_section.js
+++ app/static/js/main/detail_purchase_section.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
+    // the input blank of quantity
+    let quantity = $("#quantity-6194caf15498d8612861f033e8278855");
+    // get model id
+    let modelID = $("#btn-buy-now").attr("model_id");
+
     /* buy now */
     $("#btn-buy-now").on("click", function (){
-        //get modelID and count
-        let modelID = $("#btn-buy-now").attr("model_id");
+        //get count
         let count = $("#quantity-6194caf15498d8612861f033e8278855").val();
         console.log("count: " + count);
         //send Ajax request
@@ -20,7 +24,15 @@ $(document).ready(function(){
     });
 
     /* when changing the quantity */
-
+    quantity.on("change", function (){
+        let count = $(this).val();
+        //count number should not be empty
+        if (count === ""){
+            $(this).val(1);
+        }
+        //send ajax to compare the count with stock
+        check_stock(modelID, count);
+    });
 
 });
 
@@ -79,7 +91,32 @@ function add_to_cart(modelID, count){
             //     cart_errupter.prop('style','opacity: 1; transition: opacity 300ms ease 0s;');
             //     sidebar_cart.attr('open_',"opened");
             // }
+        }
+    });
+}
 
+function check_stock(modelID, count){
+    $.post("/api/model-detail/validate-model-count", {
+        "model_id": modelID,
+        "new_count": count
+
+    }).done(function (response){
+        // get from server (backend)
+        let returnValue = response['returnValue'];
+
+        if (returnValue === 0) {
+            //success
+
+        }else if (returnValue === 3){
+            //the count exceeds the stock
+            window.alert("Quantity exceeds the stock!")
+            $("#quantity-6194caf15498d8612861f033e8278855").val(1);
+
+        }else if (returnValue === 4){
+            //the stock becomes 0
+            //we can refresh this page, the "out of stock" statement would be shown
+            let url = "/product-details/" + modelID;
+            location.href = url;
         }
 
     });
