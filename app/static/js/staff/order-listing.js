734d790bdb073b200929a6e6c0d37a2bdac0a82a
Chenyu Guo <chenyu.guo1@ucdconnect.ie>
2022-05-04 15:15:41 +0800

    temp

--- app/static/js/staff/order-listing.js
+++ app/static/js/staff/order-listing.js
@@ -14,17 +14,40 @@ $(".priority-select").change(function()
         console.log("1");
         $(this).addClass("btn-light");
         priority_sort.html(1);
+        update_priority(orderId, 1);
     }
     else if(checkValue==='Important')
     {
         console.log("2");
         $(this).addClass("btn-info");
         priority_sort.html(2);
+        update_priority(orderId, 2);
     }
     else if(checkValue==='Significant')
     {
         console.log("3");
         $(this).addClass("btn-secondary");
         priority_sort.html(3);
+        update_priority(orderId, 3);
     }
 });
+
+function update_priority(orderId, newPriority)
+{
+    $.post("/api/order-management/update-priority",
+        {
+            "order_id": orderId,
+            "new_priority": newPriority
+        }).done(function (response)
+        {
+            //get response from server
+            let returnValue = response['returnValue'];
+
+            if (returnValue === 0)
+            {
+                console.log('successed');
+                //success
+                location.reload();
+            }
+        });
+}
