41ea80b1b194e29542b305e4be58f5c224c57153
Luca <zhe.liu1@ucdconnect.ie>
2022-05-09 21:11:35 +0800

    403 forbidden html

--- music_shop.py
+++ music_shop.py
@@ -13,9 +13,20 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 # --------------------------------------- self-defined error pages ---------------------------------------
 
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template("errors/403.html"), 403
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("errors/404.html"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("errors/500.html"), 500
+
 
 
 # --------------------------------------- context processor ---------------------------------------
