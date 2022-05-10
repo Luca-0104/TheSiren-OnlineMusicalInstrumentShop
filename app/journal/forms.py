bbaf51229f024d05f1bee3d16f3a3feb9a175f4f
Lu Zhengyang <zhengyang.lu@ucdconnect.ie>
2022-05-10 22:22:57 +0800

    update translation file

--- app/journal/forms.py
+++ app/journal/forms.py
@@ -1,21 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
+from flask_babel import lazy_gettext as _l
 
 
 class JournalUploadForm(FlaskForm):
     """
         The form for uploading journals
     """
-    title = StringField("Give a title for your journal", validators=[DataRequired(), Length(1, 128)])
-    text = TextAreaField("Write down your journal here", validators=[DataRequired(), Length(1, 5120)])
-    submit = SubmitField("Broadcast")
+    title = StringField(_l("Give a title for your journal"), validators=[DataRequired(), Length(1, 128)])
+    text = TextAreaField(_l("Write down your journal here"), validators=[DataRequired(), Length(1, 5120)])
+    submit = SubmitField(_l("Broadcast"))
 
 
 class JournalEditForm(FlaskForm):
     """
         The form for modifying journals
     """
-    title = StringField("Modify title here", validators=[DataRequired(), Length(1, 128)])
-    text = TextAreaField("Modify your journal body here", validators=[DataRequired(), Length(1, 5120)])
-    submit = SubmitField("Submit")
+    title = StringField(_l("Modify title here"), validators=[DataRequired(), Length(1, 128)])
+    text = TextAreaField(_l("Modify your journal body here"), validators=[DataRequired(), Length(1, 5120)])
+    submit = SubmitField(_l("Submit"))
