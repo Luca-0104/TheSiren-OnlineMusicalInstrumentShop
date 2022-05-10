28c5633f386e9c7d6525d78a4bfca5b908e6545b
Luca <zhe.liu1@ucdconnect.ie>
2022-05-10 21:39:29 +0800

    show comment errors

--- app/comment/forms.py
+++ app/comment/forms.py
@@ -9,7 +9,7 @@ class CommentForm(FlaskForm):
     """
         The form to upload comment for a product
     """
-    content = TextAreaField("Leave your comment here", validators=[DataRequired(), Length(1, 500)])  # comment text
+    content = TextAreaField("Leave your comment here", validators=[DataRequired("You must give some text comment."), Length(1, 500)])  # comment text
     pictures = MultipleFileField('Upload some pictures', validators=[DataRequired(), Length(2, 5, 'You must give 2-5 pictures')])  # pictures of this product
-    rate = StringField(validators=[DataRequired()])   # the rate (star num) should be put into the "value" of this field
+    rate = StringField(validators=[DataRequired("You should rate it.")])   # the rate (star num) should be put into the "value" of this field
     submit = SubmitField("Confirm")
