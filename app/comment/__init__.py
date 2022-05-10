928e4bed972e79498a24251ce07a40a1cd0653c3
Luca <zhe.liu1@ucdconnect.ie>
2022-05-10 22:04:22 +0800

    sort the comments in desc order

--- app/comment/__init__.py
+++ app/comment/__init__.py
@@ -1,6 +1,15 @@
 from flask import Blueprint
 
+from ..models import Comment
+
 # Create a Blueprint class object called comment, set the blueprint name as 'comment'
 comment = Blueprint('comment', __name__)
 
 from . import views
+
+
+@comment.app_context_processor
+def inject_classes_into_templates():
+    return dict(
+        Comment=Comment
+    )
