1d1067b1b1c6a20f2db62f9ac7bced3856a867a6
Luca <zhe.liu1@ucdconnect.ie>
2022-05-11 13:44:57 +0800

    chat_page.js staff send with enter bug fixed

--- app/static/js/chat/chat_page.js
+++ app/static/js/chat/chat_page.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Make 'enter' key submit message
-    let msg = document.querySelector('#user_message');
-    msg.addEventListener('keypress', event => {
+
+
+    /* add event listener for every message input blank */
+    $(".user-message-input-blank").keypress(function (event) {
+
+        // when pressing the "Enter" key on the keyboard
         if (event.keyCode === 13) {
+            // prevent the original usage of "Enter" key
             event.preventDefault();
-            document.querySelector('#send_message').click();
+            //get the room id
+            let roomId = $(this).attr("room-id");
+            // simulate the "send" button clicking event
+            $("#send-message-" + roomId).click();
         }
-    })
-})
+
+    });
+
+});
