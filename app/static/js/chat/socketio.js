d73ed8fae72de4fe634dc80de38d775acff78fa8
Chenyu Guo <chenyu.guo1@ucdconnect.ie>
2022-05-26 13:29:37 +0800

    jump to and animated scroll in staffs' chatting page

--- app/static/js/chat/socketio.js
+++ app/static/js/chat/socketio.js
@@ -463,3 +463,17 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#chat-window-' + chatRoomId).append(p);
     }
 });
+
+function atBottom()
+{
+    let chat_window = $("#chat-window-"+chatroom_id);
+    let h = chat_window[0].scrollHeight * 1.5;
+    chat_window.animate({scrollTop: h}, 0);
+}
+
+function toBottom()
+{
+    let chat_window = $("#chat-window-"+chatroom_id);
+    let h = chat_window[0].scrollHeight * 1.5;
+    chat_window.animate({scrollTop: h}, 2000);
+}
